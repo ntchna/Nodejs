195fa2c91d5b7492a36b81b0820861263f77d30a
Nastya Tychyna <ntchna.nastya@gmail.com>
2019-12-12 17:12:49 +0200

    0.64

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es6: true,
-        node: true,
+    "env": {
+        "es6": true
     },
-    extends: 'eslint:recommended',
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    parserOptions: {
-        ecmaVersion: 2018,
+    "parserOptions": {
+        "ecmaVersion": 2018
     },
-    rules: {},
+    "rules": {
+    }
 };
