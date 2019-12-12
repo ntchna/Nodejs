0ee9899cacf847c905cb21a5643c1cf21ccdfb5d
Nastya Tychyna <ntchna.nastya@gmail.com>
2019-12-12 17:04:57 +0200

    0.4

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-    if(res.url = '/'){
+let server = http.createServer((req, res) => {
+    if ((res.url = '/')) {
         res.setHeader('Content-Type', 'text/plain');
         res.end('Hi there!\n');
     }
