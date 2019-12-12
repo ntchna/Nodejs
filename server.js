84d6616afdef1c6ff73be67d81edbc8ca57b01aa
Nastya Tychyna <ntchna.nastya@gmail.com>
2019-12-12 17:03:53 +0200

    0.4

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hi there!\n');
+    if(res.url = '/'){
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hi there!\n');
+    }
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
